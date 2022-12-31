f26a90f10cd30cf0a1e4f3401c72fae269470572
Falco Rodenburg <falcorodenburg@gmail.com>
2022-12-31 13:49:20 +0700

    added about/bio section

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,13 +1,29 @@
-import {FC} from "react"
-import styles from "../../styles/Home.module.css"
+import { FC } from "react";
+import styles from "../../styles/Home.module.css";
 
-const About:FC = () => {
-    return (
-        <div className={styles.about}>
-            <h1>Hi, I'm Falco Rodenburg</h1>
-            <h4>Full-stack Developer</h4>
+const About: FC = () => {
+  return (
+    <div className={styles.about}>
+      <div className={styles.aboutText}>
+        <h1>Hello World</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et similique itaque ea provident nulla dolorem laudantium est labore tenetur. Quas eligendi labore culpa perferendis dolorum, perspiciatis esse? Nemo sunt nobis tempore, dolore reiciendis ab cupiditate ipsum, iusto nulla molestiae sint, quaerat recusandae necessitatibus. Recusandae repudiandae, eligendi nulla corrupti veniam ut eaque harum id magni dolor atque aspernatur numquam error dolorem?
+        </p>
+      </div>
+      <div className={styles.bio}>
+        <h1>Bio</h1>
+        <div>
+        <ul>
+            <li><span>1990</span><p>Born in Wirdum, The Nederlands</p></li>
+            <li><span>2015</span> Completed Master's degree in Psychology at Rijksuniversiteit Groningen</li>
+            <li><span>2018</span> Started to learn programming</li>
+            <li><span>2019</span> Worked at IBM Netherlands until 2020</li>
+            <li><span>2022</span> Started working as freelance developer</li>
+        </ul>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,24 +12,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Navbar /> */}
       <main className={styles.main}>
         <div className={styles.container}>
           <Navbar />
-        {/* <section id="about">
-          <h2>/ About</h2>
-          <About />
-        </section> */}
-        <section id="works">
-          {/* <h2>/ Projects</h2> */}
-          <Works />
-        </section>
-        {/* <section>
-          <h2>/ Contact</h2>
-        </section> */}
+          <section id="works">
+            <About />
+          </section>
         </div>
       </main>
-
     </>
   );
 }
