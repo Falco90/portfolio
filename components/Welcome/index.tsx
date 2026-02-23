ae578626d6c0d57a31c068fb22d49c7233fa94e0
Falco90 <falcorodenburg@gmail.com>
2026-02-23 19:01:10 +0700

    setup vite project

--- components/Welcome/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-const Welcome: FC = () => {
-  return <div>Welcome to my page</div>;
-};
-
-export default Welcome;
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './style.css'
+import viteLogo from '/vite.svg'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Falco Rodenburg</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
