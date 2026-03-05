f1bbbebeb26cecf0885968d3af04211b5c5dddaa
Falco90 <falcorodenburg@gmail.com>
2026-03-05 16:54:12 +0700

    update projects section with accordion

--- src/main.js
+++ src/main.js
@@ -14,6 +14,41 @@ splitText('h1', {
     delay: stagger(200),
   }));
 
+const items = document.querySelectorAll(".accordion-item");
+
+items.forEach(item => {
+  const button = item.querySelector(".accordion-title");
+  const title = item.querySelector((".project-title"));
+  const content = item.querySelector(".accordion-content");
+
+  button.addEventListener("click", () => {
+
+    const isOpen = item.classList.contains("active");
+
+    // Close all
+    items.forEach(i => {
+      title.style.transform = `translateX(0)`;
+      i.classList.remove("active");
+      const c = i.querySelector(".accordion-content");
+      c.style.height = "0px";
+    });
+
+    const containerWidth = button.offsetWidth;
+    const textWidth = title.offsetWidth;
+
+    const distance = containerWidth - textWidth;
+
+    title.style.transform = `translateX(-${distance}px)`;
+    // If it was closed, open it
+    if (!isOpen) {
+      item.classList.add("active");
+      content.style.height = content.scrollHeight + "px";
+    }
+
+
+  });
+});
+
 document.getElementById("nav").addEventListener("click", (e) => {
   console.log("clicked");
   const link = e.target.closest("a");
