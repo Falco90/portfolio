80a010d2985382b941bd0523047b2b31b386ae8d
Falco90 <falcorodenburg@gmail.com>
2026-03-06 12:51:44 +0700

    update design experience section

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import './style.css';
 
-import { animate, splitText, stagger } from 'animejs';
+document.addEventListener("DOMContentLoaded", () => {
+  const initialActiveItem = document.querySelector(".accordion-item.active");
 
-splitText('h1', {
-  lines: { wrap: 'clip' },
-})
-  .addEffect(({ lines }) => animate(lines, {
-    y: [
-      { to: ['100%', '0%'] },
-    ],
-    duration: 750,
-    ease: 'out(3)',
-    delay: stagger(200),
-  }));
+  if (initialActiveItem) {
+    openItem(initialActiveItem)
+  }
+});
 
 const items = document.querySelectorAll(".accordion-item");
 
